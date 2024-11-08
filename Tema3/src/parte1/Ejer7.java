a0b4b2fc26140e42ccb6bdde15e0617d5a90222c
guillevr777 <gs.villanueva@iesnervion.es>
2024-11-08 14:20:24 +0100

    All done

--- Tema3/src/parte1/Ejer7.java
+++ Tema3/src/parte1/Ejer7.java
@@ -4,8 +4,23 @@ import java.util.Scanner;
 
 public class Ejer7 {
 
-	public static void main(String[] args) {
-		//creamos el escaner
+	public static void main(String[]args) {
 		Scanner reader = new Scanner(System.in);
+		
+		int numeroU = 0;
+		
+		int numero [] = new int [12];
+		
+		System.out.println("Dime las temperaturas de todos los meses del año de 1 en 1 :");
+		
+		for (int i=0 ; i<numero.length ; i++) {			
+			//preguntamos por la temperaturas de este año
+			System.out.println("Dime una :");
+			numeroU = reader.nextInt();
+			
+			numero[i] = numeroU;
+			
+			}
+		System.out.println("*");
 	}
 }
---
+++ Tema3/src/parte1/Ejer8.java
@@ -0,0 +1,50 @@
+package parte1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejer8 {
+
+	public static void main(String[]args) {
+		
+			//creamos el escaner
+			Scanner reader = new Scanner(System.in);
+			
+			//creo la clase random
+			Random random = new Random();
+			
+			//creo una variable para el numero de busquedas
+			int indiceBusqueda = 0;
+			
+			//numero que busco
+			int numeroBuscado = 0;
+			
+			//cantidad de numero elegido repetidos
+			int cantidad = 0;
+			
+			//creamos la tabla
+			int numero [] = new int [100];
+			
+			//preguntamos por el numero que quiere que busque las posiciones
+			System.out.println("Que numero quieres que te encuentre? *Del 1 al 10*");
+			numeroBuscado = reader.nextInt();
+			
+			//Le comunicamos al usuario donde se encuentra el numero dicho en las posiciones
+			System.out.print("El numero que busca se encuentra en las siguientes posiciones :");
+						
+			//asignamos un numero del 1 al 10 a cada posicion de la tabla
+			for (int i=0 ; i<numero.length ; i++) {	
+				
+				numero[i] = random.nextInt(1,11);
+				
+				if (numero[i] == numeroBuscado) {
+					System.out.print(i + " , ");
+					cantidad++;
+				}
+			}
+				System.out.println("Habian " + cantidad + " numeros de " + numeroBuscado);	
+			
+		//cerramos el escaner
+			reader.close();
+		}
+	}
