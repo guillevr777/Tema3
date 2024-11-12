fd701e5c1792d91f0ba38642ac7f04692b2c6fad
guillevr777 <gs.villanueva@iesnervion.es>
2024-11-12 13:32:35 +0100

    Done

--- Tema3/src/englishexercises/Exer3.java
+++ Tema3/src/englishexercises/Exer3.java
@@ -1,5 +1,56 @@
 package englishexercises;
 
-public class Exer3 {
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Exer3 {
+	public static void main(String[]args) {
+		
+		//we create the scanner to read the console
+		Scanner reader = new Scanner (System.in);
+		
+		//creamos el random
+		Random random = new Random();
+		
+		//created a boolean to know when can we continue the program or we need to restart it
+		boolean continuar = true;
+		
+		//created vairables for values and spaces
+		int value = 0;
+		int incremento = 0;
+		int length = 10;
+		
+		//created a variable to use the boolean
+		int aleatorio;
+		
+		try {
+		
+		//we create the table with a size of 10 numbers
+		int numbers[] = new int[length];
+		
+		while (continuar == false) {
+			
+			aleatorio = random.nextInt(0,10);
+			numbers[aleatorio] = aleatorio;
+		
+		//pendiente de hacer
+		if (numbers < 0) {
+			
+		}
+		}
+		
+		System.out.println(Arrays.toString(numbers));
+		
+		} catch (InputMismatchException e) {
+            System.out.println("Error: Por favor ingrese un número válido.");
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error inesperado: " + e.getMessage());
+        } finally {
+		
+	//we close the scanner
+	reader.close();
+        }
+	}
 }
