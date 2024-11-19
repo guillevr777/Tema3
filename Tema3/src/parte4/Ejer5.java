99ef56169cbad37f76630bf68e8f12aad52209a7
guillevr777 <gs.villanueva@iesnervion.es>
2024-11-19 20:26:12 +0100

    Ejercicios unidimencionales con explicacion

--- Tema3/src/parte4/Ejer5.java
+++ Tema3/src/parte4/Ejer5.java
@@ -6,49 +6,66 @@ import java.util.Scanner;
 public class Ejer5 {
 	
 	public static void main(String[]args) {
+		
+		//creamos el escaner
 		Scanner reader = new Scanner(System.in);
 
+		//creamos la tabla
 		int tabla[] = {1, 2 , 3, 4, 5, 2, 2, 1, 1, 5, 2, 4, 3, 1, 2};
 		
+		//pedimos el valor
 		System.out.println("Dime un valor a buscar:");
 		int valor = reader.nextInt();
 		
+		//creamos tabla con los valores de la funcion buscarTodos
 		int[] indices = buscarTodos(tabla, valor);
 		
+		//nos aseguramos de que la tabla tenga minimo 1 valor
 		if (indices.length == 0) {
 			System.out.println("No se encuentra el valor");
 		} else {
 			System.out.println("DIcho valor se encuentra en :" + Arrays.toString(indices));
 		}
+		//cerramos el escaner
 		reader.close();
 	}
+	//creamos la funcion buscarTodos para encontrar todos los indices con dicho valor
 	static int[] buscarTodos(int t[], int valor) {
+		
+		//creamos variable tamaño para crear la tabla con una longitud especifica
 		int tamaño = numVeces(t,valor);
 		
+		//si es 0 devuelve tabla vacia
 		if (tamaño == 0) {
 			return new int[0];
 		}
 		
+		//sino crea tabla con la medida 
 		int[] indices = new int [tamaño];
 		int indiceActual = 0;
 		
+		//creamos for para recorrer la tabla , en caso de encontrar un valor igual al buscado guarda el indice en la tabla
 		for (int i = 0 ; i < t.length ; i++) {
 			if (t[i] == valor) {
 				indices[indiceActual] = i;
 				indiceActual++;
 			}
 		}
-		
+		//devuelve la tabla indices
 		return indices;
 	}
+	//creamos una funcion para contar el numero de indices que cuentan con el valor que queremos
 	static int numVeces(int t[], int valor) {
+		//se crea un contador 
 		int contador = 0;
 		
+		//creamos un for para recorrer la tabla e incrementar el valor del contador en caso de encontrar el valor buscado
 		for (int contenido : t) {
 			if (contenido == valor) {
 				contador++;
 			}
 		}
+		//se devuelve el valor del contador
 		return contador;
 	}
 }
--- Tema3/src/parte4/Ejer6.java
+++ Tema3/src/parte4/Ejer6.java
@@ -6,31 +6,40 @@ public class Ejer6 {
 
 	public static void main(String[]args) {
 		
+		//creamos tabla de enteros
 		 int tablaEnteros[] = {5, 6, 7, 3, 4, 2, 1, 2, 9, 8};
 		 
+		 	//creamos variable que definira el conjunto de numeros a sumar por iteracion
 	        int numElementos = 3;
 	        
+	        //creamos tabla para almacenar las sumas
 	        int[] sumaEnteros = suma(tablaEnteros, numElementos);
 	        
+	        //mostramos la tabla
 	        System.out.println(Arrays.toString(sumaEnteros));
 	        
 	 }
+	//creamos funcion encargada de la suma
 	static int[] suma(int t[], int numElementos) {
 		
+		//creamos variable para controlar la longitud
         int numSumas = t.length - numElementos + 1;
         
+        //creamos tabla resultado para contener las sumas , se le asigna numSumas como longitud
         int[] resultado = new int[numSumas];
         
+        //creamos for para sumar los valores de los indices
         for (int i = 0; i < numSumas; i++) {
         	
             int suma = 0;
             
+            //bucle for para repetirse las veces que se indique
             for (int j = 0; j < numElementos; j++) {
                 suma += t[i + j];
             }
             resultado[i] = suma;
         }
-        
+        //se devuelve la tabla resultado
         return resultado;
     }
 }
